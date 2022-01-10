4145397c8c4da8ba4d403a62ffd7d8cf4cb2505e
manish0502 <manishgoswami495@gmail.com>
2022-01-10 18:11:02 +0530

    added

--- typeorm-relations-nestjs-2/src/meetings/meetings.respository.ts
+++ typeorm-relations-nestjs-2/src/meetings/meetings.respository.ts
@@ -5,5 +5,5 @@ import {EntityRepository} from "typeorm/decorator/EntityRepository";
 import {meetingEntity} from './meetings.entity';
 
 @EntityRepository(meetingEntity)
-export class TaskInfoRepository extends Repository<meetingEntity> {
+export class meetingsRepository extends Repository<meetingEntity> {
 }
